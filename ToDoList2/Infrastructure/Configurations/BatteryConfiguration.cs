b5cfd97ea3017436ae4d31dbb0a9a3fee19405d1
Akilov <akilovmunirjon@gmail.com>
2023-10-26 17:41:08 +0500

    ashdqowh

--- ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
+++ ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
@@ -17,7 +17,7 @@ namespace ToDoList2.Infrastructure.Configurations
             builder.Property(b => b.Capacity)
                    .IsRequired();
 
-            builder.HasMany(b => b.CreatedAt)
+            builder.HasMany(b => b.Capacity)
                    .WithOne(p => p.Battery)
                    .HasForeignKey(p => p.BatteryId);
         }
