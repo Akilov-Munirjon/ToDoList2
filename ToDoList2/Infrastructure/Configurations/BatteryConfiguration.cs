39060609d8d02d108b88cdb2736c6aa526169d1e
Akilov <akilovmunirjon@gmail.com>
2023-10-27 15:23:44 +0500

    dasda

--- ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
+++ ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
@@ -17,9 +17,9 @@ namespace ToDoList2.Infrastructure.Configurations
             builder.Property(b => b.Capacity)
                    .IsRequired();
 
-            //builder.HasMany(b => b.Capacity)
-            //       .WithOne(p => p.Battery)
-            //       .HasForeignKey(p => p.BatteryId);
+            builder.HasMany(b => b.Capacity)
+                   .WithOne(p => p.Battery)
+                   .HasForeignKey(p => p.BatteryId);
         }
     }
 }
