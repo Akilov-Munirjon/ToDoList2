4bce9f3a2bd73119f783af206f780281dfa56b28
Akilov <akilovmunirjon@gmail.com>
2023-10-30 13:12:20 +0500

    изменение DisplayConfiguration

--- ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
+++ ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
@@ -17,6 +17,7 @@ namespace ToDoList2.Infrastructure.Configurations
             builder.Property(b => b.Capacity)
                    .IsRequired();
 
+
             builder.HasMany(b => b.Baterry)
                    .WithOne(p => p.Battery)
                    .HasForeignKey(p => p.BatteryId);
