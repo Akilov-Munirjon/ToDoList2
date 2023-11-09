1e73fd1baf4557b705b866d261a04c692ab774ef
Akilov-Munirjon <akilovmunirjon@gmail.com>
2023-11-09 11:37:16 +0500

    Изминение конфигурации

--- ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
+++ ToDoList2/Infrastructure/Configurations/BatteryConfiguration.cs
@@ -17,7 +17,7 @@ namespace ToDoList2.Infrastructure.Configurations
             builder.Property(b => b.Capacity)
                    .IsRequired();
 
-            builder.HasMany(b => b.Capacity)
+            builder.HasMany(p => p.Phones)
                    .WithOne(p => p.Battery)
                    .HasForeignKey(p => p.BatteryId);
         }
